e136b60938dc18897730d1b1ab9ef758b4ecaa56
Apoorve Verma <vapoorve@gmail.com>
2023-06-07 02:23:31 +0530

    DSA#1 Set Matrix Zeroes Optimal Approach

--- Arrays/Set Matrix Zeroes.js
+++ Arrays/Set Matrix Zeroes.js
@@ -1,6 +1,9 @@
 // Set Matrix Zeroes
 // https://leetcode.com/problems/set-matrix-zeroes/
 
+// Brute Force Approach:
+// Time Complexity: O(n * m)
+// Space Complexity: O(n + m)
 const setZeroes = (matrix) => {
   const rowLen = matrix.length;
   const colLen = matrix[0].length;
@@ -24,4 +27,48 @@ const setZeroes = (matrix) => {
       }
     }
   }
+};
+
+// Optimal Approach:
+// Time Complexity: O(n * m)
+// Space Complexity: O(1)
+const setZeroes = (matrix) => {
+  const n = matrix.length;
+  const m = matrix[0].length;
+  let col0 = 1;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0;
+        if (j !== 0) {
+          matrix[0][j] = 0;
+        } else {
+          col0 = 0;
+        }
+      }
+    }
+  }
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < m; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  if (matrix[0][0] === 0) {
+    for (let j = 0; j < m; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+
+  if (col0 === 0) {
+    for (let i = 0; i < n; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+
+  return matrix;
 };
